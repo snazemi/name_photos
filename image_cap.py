33c5e3c6a95243a6a8f66e6433acbc8ac7db1619
Soroush Nazemi <soroush.nazemi@gmail.com>
2025-02-10 19:59:51 -0600

    added the image of 2 dogs

--- image_cap.py
+++ image_cap.py
@@ -7,7 +7,7 @@ processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 # Load your image, DONT FORGET TO WRITE YOUR IMAGE NAME
-img_path = "YOUR IMAGE NAME.jpeg"
+img_path = "two-dogs.jpeg"
 # convert it into an RGB format 
 image = Image.open(img_path).convert('RGB')
 
